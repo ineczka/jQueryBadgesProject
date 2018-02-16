0217c466d59f97e0e11ec6f52474c535e3dce9a0
Iwona <ibaczar@hotmail.com>
2018-02-16 03:48:58 -0500

    first commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
 
-  // your code will go here
-
+  $.ajax( {
+    url : 'https://www.codeschool.com/users/i_nka2001.json',
+    dataType: 'jsonp',
+    success: function(response){
+      $.each(response.courses.completed, function(index, elem){
+         var button = $('<a href='+  elem.url+' target = blank ></a>').addClass('btn btn-primary').text("See Course"),
+             title = $('<h3>'+  elem.title+ '</h3>'),
+             image = $('<img>').attr('src', elem.badge),
+             course = ("<div></div>").addClass('course'),
+             badge = course.append(title, image, button);
+        $('#badges').append(course);
+      });
+    }
+  })
 });
